2c0b4d09170571077c058456d3b3328913d7bec0
dsalvatim <david.salvatierramartin@hotmail.com>
2023-06-27 21:25:14 +0200

    Update acr.tf

--- Terraform/CentOS/acr.tf
+++ Terraform/CentOS/acr.tf
@@ -1,8 +1,3 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_container_registry" "acr" {
   name                = var.registry_name
   resource_group_name = azurerm_resource_group.rg.name
