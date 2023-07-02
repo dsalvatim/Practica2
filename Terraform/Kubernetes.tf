91ca8ab2edf626a0bb9fc7cc984a833ba4e01201
dsalvatim <david.salvatierramartin@hotmail.com>
2023-07-02 21:08:00 +0200

    Update Kubernetes.tf

--- Terraform/Kubernetes.tf
+++ Terraform/Kubernetes.tf
@@ -20,12 +20,12 @@ resource "azurerm_kubernetes_cluster" "KubCluster" {
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.rg.kube_config.0.client_certificate
+  value     = azurerm_kubernetes_cluster.KubCluster.kube_config.0.client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.rg.kube_config_raw
+  value = azurerm_kubernetes_cluster.KubCluster.kube_config_raw
 
   sensitive = true
 }
