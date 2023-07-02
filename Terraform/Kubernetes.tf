6d3a35126ca4a075a337d91e805dda1b7e8995a6
dsalvatim <david.salvatierramartin@hotmail.com>
2023-07-02 20:55:04 +0200

    Create Kubernetes.tf

---
+++ Terraform/Kubernetes.tf
@@ -0,0 +1,36 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "KubCluster" {
+  name                = "MiClusterKubernetes"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "MiKubernetesCluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.rg.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.rg.kube_config_raw
+
+  sensitive = true
+}
